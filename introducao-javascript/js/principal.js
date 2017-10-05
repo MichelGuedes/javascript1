24fe18905befda84d5cfee0e33337edbb238ef3e
Guedes <michel.guedes@recall.com>
2017-10-05 10:56:29 -0300

    Criando o cálculo e validação inicial do IMC, aula 2 do curso de JS1

--- introducao-javascript/js/principal.js
+++ introducao-javascript/js/principal.js
@@ -1,2 +1,28 @@
 var titulo = document.querySelector("h1.titulo");
 titulo.textContent = "Aparecida Nutricionista";
+
+var primeiropaciente = document.querySelector("#primeiro-paciente");
+var alturaPaciente = document.querySelector(".info-altura").textContent;
+var pesoPaciente = document.querySelector(".info-peso").textContent;
+
+var alturaValida = true;
+var pesoValido = true;
+
+if (pesoPaciente <=0 || pesoPaciente >= 300){
+  pesoValido = false;
+  var tdPeso = primeiropaciente.querySelector(".info-peso");
+  tdPeso.textContent("Peso inválido!");
+}
+
+if (alturaPaciente <=0 || alturaPaciente >= 3.00){
+  alturaValida = false;
+  var tdAltura = primeiropaciente.querySelector(".info-altura");
+  tdAltura.textContent("Altura inválida!");
+}
+
+//calcula IMC ou não dependendo dos valores de peso e altura válidos
+if (alturaValida && pesoValido){
+  var imc = pesoPaciente / (alturaPaciente * alturaPaciente);
+  var tdImc = primeiropaciente.querySelector(".info-imc");
+  tdImc.textContent = imc;
+}
