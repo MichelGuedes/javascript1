51f120cb8d1d0f93511ba63a80dcce6cad99f552
Guedes <michel.guedes@recall.com>
2017-10-05 13:44:11 -0300

    classe pacienteinvalido adicionada em index.css, bg vermelho em prin.js

--- introducao-javascript/js/principal.js
+++ introducao-javascript/js/principal.js
@@ -1,28 +1,36 @@
 var titulo = document.querySelector("h1.titulo");
 titulo.textContent = "Aparecida Nutricionista";
 
-var primeiropaciente = document.querySelector("#primeiro-paciente");
-var alturaPaciente = document.querySelector(".info-altura").textContent;
-var pesoPaciente = document.querySelector(".info-peso").textContent;
+var pacientes = document.querySelectorAll(".paciente");
 
-var alturaValida = true;
-var pesoValido = true;
+for (i = 0; i <= pacientes.length; i++){
 
-if (pesoPaciente <=0 || pesoPaciente >= 300){
-  pesoValido = false;
-  var tdPeso = primeiropaciente.querySelector(".info-peso");
-  tdPeso.textContent("Peso inválido!");
-}
+  //var primeiropaciente = document.querySelector("#primeiro-paciente");
+  var alturaPaciente = pacientes[i].querySelector(".info-altura").textContent;
+  var pesoPaciente = pacientes[i].querySelector(".info-peso").textContent;
 
-if (alturaPaciente <=0 || alturaPaciente >= 3.00){
-  alturaValida = false;
-  var tdAltura = primeiropaciente.querySelector(".info-altura");
-  tdAltura.textContent("Altura inválida!");
-}
+  var alturaValida = true;
+  var pesoValido = true;
 
-//calcula IMC ou não dependendo dos valores de peso e altura válidos
-if (alturaValida && pesoValido){
-  var imc = pesoPaciente / (alturaPaciente * alturaPaciente);
-  var tdImc = primeiropaciente.querySelector(".info-imc");
-  tdImc.textContent = imc;
+  if (pesoPaciente <=0 || pesoPaciente >= 300){
+    pesoValido = false;
+    var tdPeso = pacientes[i].querySelector(".info-peso");
+    tdPeso.textContent = "Peso inválido!";
+  }
+
+  if (alturaPaciente <=0 || alturaPaciente >= 3.00){
+    alturaValida = false;
+    var tdAltura = pacientes[i].querySelector(".info-altura");
+    tdAltura.textContent = "Altura inválida!";
+  }
+
+  //calcula IMC ou não dependendo dos valores de peso e altura válidos
+  if (alturaValida && pesoValido){
+    var imc = pesoPaciente / (alturaPaciente * alturaPaciente);
+    var tdImc = pacientes[i].querySelector(".info-imc");
+    tdImc.textContent = imc.toFixed(2);
+  }
+  else {
+    pacientes[i].classList.add("paciente-invalido");
+  }
 }
