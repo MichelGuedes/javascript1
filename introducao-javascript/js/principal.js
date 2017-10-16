cfea484af9be6b2731ca325ed663b506b58637b5
MichelGuedes <michel.guedes.vieira@gmail.com>
2017-10-16 09:50:35 -0200

    eu nao lembro em que ponto parei, sorry

--- introducao-javascript/js/principal.js
+++ introducao-javascript/js/principal.js
@@ -3,9 +3,8 @@ titulo.textContent = "Aparecida Nutricionista";
 
 var pacientes = document.querySelectorAll(".paciente");
 
-for (i = 0; i <= pacientes.length; i++){
+for (i = 0; i < pacientes.length; i++){
 
-  //var primeiropaciente = document.querySelector("#primeiro-paciente");
   var alturaPaciente = pacientes[i].querySelector(".info-altura").textContent;
   var pesoPaciente = pacientes[i].querySelector(".info-peso").textContent;
 
@@ -34,3 +33,43 @@ for (i = 0; i <= pacientes.length; i++){
     pacientes[i].classList.add("paciente-invalido");
   }
 }
+
+/*secao para tratamento do formulario "form-adiciona" dentro de index.html*/
+var botaoAdicionar = document.querySelector("#adicionar-paciente");
+botaoAdicionar.addEventListener("click", function(event){
+  //previne o reload do form no metodo abaixo
+  event.preventDefault();
+
+  //captura os valores dos campos texto no formulario
+  var form = document.querySelector("#form-adiciona");
+  var nome = form.nome.value;
+  var peso = form.peso.value;
+  var altura = form.altura.value;
+  var gordura = form.gordura.value;
+
+  //cria uma nova tr
+  var pacienteTr = document.createElement("tr");
+
+  //cria uma td nova para cada campo
+  var nomePacienteTd = document.createElement("td");
+  var pesoPacienteTd = document.createElement("td");
+  var alturaPacienteTd = document.createElement("td");
+  var gorduraPacienteTd = document.createElement("td");
+  var imcPacienteTd = document.createElement("td");
+
+  //passa os valores capturados nas textboxes para as tags tds criadas acima
+  nomePacienteTd.textContent = nome;
+  pesoPacienteTd.textContent = peso;
+  alturaPacienteTd.textContent = altura;
+  gorduraPacienteTd.textContent = gordura;
+
+  //associa as tds criadas com o tr principal criado
+  pacienteTr.appendChild(nomePacienteTd);
+  pacienteTr.appendChild(pesoPacienteTd);
+  pacienteTr.appendChild(alturaPacienteTd);
+  pacienteTr.appendChild(gorduraPacienteTd);
+
+  //associa a nova linha tr com a tabela do form
+  var tabelaPacientes = document.querySelector("#tabela-pacientes");
+  tabelaPacientes.appendChild(pacienteTr);
+});
