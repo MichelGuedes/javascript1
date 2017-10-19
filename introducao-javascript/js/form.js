78a815454c385bead41141d3fa08c6e546b95320
Guedes <michel.guedes@recall.com>
2017-10-19 10:08:41 -0200

    fim da aula 5, arquivo principal separado em dois, add de funcoes

--- introducao-javascript/js/form.js
+++ introducao-javascript/js/form.js
@@ -1,32 +1,45 @@
 /*secao para tratamento do formulario "form-adiciona" dentro de index.html*/
 var botaoAdicionar = document.querySelector("#adicionar-paciente");
-botaoAdicionar.addEventListener("click", function(event){
-  //previne o reload do form no metodo abaixo
-  event.preventDefault();
 
-  //captura os valores dos campos texto no formulario
-  var form = document.querySelector("#form-adiciona");
-  var nome = form.nome.value;
-  var peso = form.peso.value;
-  var altura = form.altura.value;
-  var gordura = form.gordura.value;
+////////////////////////////////////////////////////////////////////////////////
 
+//pega os dados de um paciente, a partir de um form parametro
+function retornaPacienteFormulario(form){
+  var paciente = {
+    nome: form.nome.value,
+    peso: form.peso.value,
+    altura: form.altura.value,
+    gordura: form.gordura.value,
+    imc: calculaImc(form.peso.value, form.altura.value)
+  }
+
+  return paciente;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+//cria uma tag TD e passa os atributos de valor e classe para ela
+function adicionaTd(valor, classe){
+  var td = document.createElement("td");
+  td.classList.add(classe);
+  td.textContent = valor;
+  return td;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+//adiciona a tag TR a um paciente parametro
+function adicionaTr(paciente){
   //cria uma nova tr
   var pacienteTr = document.createElement("tr");
+  pacienteTr.classList.add("paciente");
 
   //cria uma td nova para cada campo
-  var nomePacienteTd = document.createElement("td");
-  var pesoPacienteTd = document.createElement("td");
-  var alturaPacienteTd = document.createElement("td");
-  var gorduraPacienteTd = document.createElement("td");
-  var imcPacienteTd = document.createElement("td");
-
-  //passa os valores capturados nas textboxes para as tags tds criadas acima
-  nomePacienteTd.textContent = nome;
-  pesoPacienteTd.textContent = peso;
-  alturaPacienteTd.textContent = altura;
-  gorduraPacienteTd.textContent = gordura;
-  imcPacienteTd.textContent = calculaImc(peso, altura);
+  var nomePacienteTd = adicionaTd(paciente.nome, "info-nome");
+  var pesoPacienteTd = adicionaTd(paciente.peso, "info-peso");
+  var alturaPacienteTd = adicionaTd(paciente.altura, "info-altura");
+  var gorduraPacienteTd = adicionaTd(paciente.gordura, "info-nome");
+  var imcPacienteTd = adicionaTd(paciente.imc, "info-imc");
 
   //associa as tds criadas com o tr principal criado
   pacienteTr.appendChild(nomePacienteTd);
@@ -35,7 +48,30 @@ botaoAdicionar.addEventListener("click", function(event){
   pacienteTr.appendChild(gorduraPacienteTd);
   pacienteTr.appendChild(imcPacienteTd);
 
+  return pacienteTr;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+//adiciona um listener de click no botao de adicionar do form
+botaoAdicionar.addEventListener("click", function(event){
+  //previne o reload do form no metodo abaixo
+  event.preventDefault();
+
+  //seleciona o formulario de cadastro do paciente do HTML
+  var form = document.querySelector("#form-adiciona");
+
+  //extrai os dados do paciente que esta no formulario e popula a variavel obj
+  //paciente abaixo
+  var paciente = retornaPacienteFormulario(form);
+
+  //retorna o Tr montado para popular na tabela HTML
+  var pacienteTr = adicionaTr(paciente);
+
   //associa a nova linha tr com a tabela do form
   var tabelaPacientes = document.querySelector("#tabela-pacientes");
   tabelaPacientes.appendChild(pacienteTr);
+
+  //limpa os campos do formulario
+  form.reset();
 });
