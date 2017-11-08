fd7c062aa9e5adc13c94dc3db295fef848498338
Guedes <michel.guedes@recall.com>
2017-11-08 11:05:13 -0200

    Adicao de tratamento de erro de conexao para dados JSON

--- introducao-javascript/js/buscar-pacientes.js
+++ introducao-javascript/js/buscar-pacientes.js
@@ -3,16 +3,27 @@ var botaoMaisPacientes = document.querySelector("#buscar-pacientes");
 botaoMaisPacientes.addEventListener("click", function(){
   //cria um objeto capaz de ler uma request em JSON e passa a URI com os dados
   var xhr = new XMLHttpRequest();
-  xhr.open("GET", "https://api-pacientes.herokuapp.com/pacientes");
+  xhr.open("GET", "https://api1-pacientes.herokuapp.com/pacientes");
 
-  //se houver o "load" de dados no objeto da conexao...
   xhr.addEventListener("load", function(){
-    //...carrega todos os pacientes encontrados em XHR e parseia para o formato JSON
-    var pacientes = JSON.parse(xhr.responseText);
+    //se houver o "load" de dados no objeto da conexao...
+    //status 200 = sucesso na conexao
+    if (xhr.status == 200){
 
-    pacientes.forEach(function(paciente){
-      adicionaPaciente(paciente); //-> essa funcao esta dentro de form.js
-    });
+      //...carrega todos os pacientes encontrados em XHR e parseia para o formato JSON
+      var pacientes = JSON.parse(xhr.responseText);
+
+      pacientes.forEach(function(paciente){
+        adicionaPaciente(paciente); //-> essa funcao esta dentro de form.js
+      });
+    }
+
+    //erro na conexao para pegar os dados
+    else {
+      console.log(xhr.status);
+      console.log(xhr.responseText);
+    }
+
   });
 
   xhr.send();
