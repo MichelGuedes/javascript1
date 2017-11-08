502f6f177b93dc6cb4696add195bef99be99702f
Guedes <michel.guedes@recall.com>
2017-11-08 10:54:51 -0200

    Buscar pacientes ja consegue carregar JSON externo na tabela do index

--- introducao-javascript/js/buscar-pacientes.js
+++ introducao-javascript/js/buscar-pacientes.js
@@ -1,18 +1,18 @@
 var botaoMaisPacientes = document.querySelector("#buscar-pacientes");
 
 botaoMaisPacientes.addEventListener("click", function(){
-  console.log("Procurando pacientes...");
+  //cria um objeto capaz de ler uma request em JSON e passa a URI com os dados
   var xhr = new XMLHttpRequest();
   xhr.open("GET", "https://api-pacientes.herokuapp.com/pacientes");
 
+  //se houver o "load" de dados no objeto da conexao...
   xhr.addEventListener("load", function(){
-      console.log(xhr.responseText);
-      var r = xhr.responseText;
-      console.log(typeof r);
+    //...carrega todos os pacientes encontrados em XHR e parseia para o formato JSON
+    var pacientes = JSON.parse(xhr.responseText);
 
-      var pacientes = JSON.parse(r);
-      console.log(pacientes);
-      console.log(typeof pacientes);
+    pacientes.forEach(function(paciente){
+      adicionaPaciente(paciente); //-> essa funcao esta dentro de form.js
+    });
   });
 
   xhr.send();
