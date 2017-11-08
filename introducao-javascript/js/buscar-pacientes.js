5867eabf8ddc9d6963e911291dff385f482ac1f5
Guedes <michel.guedes@recall.com>
2017-11-08 11:23:23 -0200

    Finalizacao do projeto, add tratamento de erro e msg na tela

--- introducao-javascript/js/buscar-pacientes.js
+++ introducao-javascript/js/buscar-pacientes.js
@@ -3,15 +3,17 @@ var botaoMaisPacientes = document.querySelector("#buscar-pacientes");
 botaoMaisPacientes.addEventListener("click", function(){
   //cria um objeto capaz de ler uma request em JSON e passa a URI com os dados
   var xhr = new XMLHttpRequest();
-  xhr.open("GET", "https://api1-pacientes.herokuapp.com/pacientes");
+  xhr.open("GET", "https://api-pacientes.herokuapp.com/pacientes");
 
   xhr.addEventListener("load", function(){
     //se houver o "load" de dados no objeto da conexao...
     //status 200 = sucesso na conexao
-    if (xhr.status == 200){
+    var erroAjax = document.querySelector("#erro-ajax");
 
+    if (xhr.status == 200){
       //...carrega todos os pacientes encontrados em XHR e parseia para o formato JSON
       var pacientes = JSON.parse(xhr.responseText);
+      erroAjax.classList.add("invisivel");
 
       pacientes.forEach(function(paciente){
         adicionaPaciente(paciente); //-> essa funcao esta dentro de form.js
@@ -20,8 +22,7 @@ botaoMaisPacientes.addEventListener("click", function(){
 
     //erro na conexao para pegar os dados
     else {
-      console.log(xhr.status);
-      console.log(xhr.responseText);
+        erroAjax.classList.remove("invisivel");
     }
 
   });
